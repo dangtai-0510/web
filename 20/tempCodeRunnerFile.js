74c23eaf049b4b6044daddcabd8f5e0d787aeb5d
Đặng Tài <dangductai0510@gmail.com>
2026-04-03 03:36:37 +0000

    delete file

--- 20/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-// Giai đoạn 2 – nền hồng pastel nhẹ
-  body.animate(
-    [
-      { background: "linear-gradient(135deg,#ffd6e7,#fff0f5)" },
-      { background: "linear-gradient(135deg,#ffe1f0,#fff4f7)" },
-    ],
-    { duration: 2000, fill: "forwards" }
-  );
--- test/js/router.js
+++
@@ -1,9 +0,0 @@
-const Router = {
-  go(html){ document.getElementById('view-container').innerHTML = html; },
-  nav(user=''){
-    document.getElementById('nav-right').innerHTML = user
-      ? `<div class="navbar-item">Xin chào, <strong>${user}</strong></div>
-         <a class="navbar-item" onclick="Router.logout()">Đăng xuất</a>` : '';
-  },
-  logout(){ sessionStorage.removeItem('curUser'); Auth.showLogin(); }
-};
